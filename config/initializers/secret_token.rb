29893cef7f92aa6466fd98ea2e6a9be29c9201de
saasbook <gabal@ubuntu.(none)>
2014-02-14 16:15:37 -0800

    Initial Commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ParksFirstApp2::Application.config.secret_key_base = 'b0b195fd5180437bf6bc9f34fe3717e618b0d4acc9b97db264f4b52105494b00483c0bd02e56963e94c8d18fba45606d232ccf488d6ce0b9a7123a55450cf5db'
